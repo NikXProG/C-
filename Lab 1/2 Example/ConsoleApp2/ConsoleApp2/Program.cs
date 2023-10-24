5955c2c5b7c2ca1e93e470a785de54a1262fa9e4
NikX <89803680+NikXProG@users.noreply.github.com>
2023-10-24 05:18:55 +0300

    commit

---
+++ Lab 1/2 Example/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception("Please provide an epsilon ONE value as a command line argument.");
+                }
+
+                if (!Double.TryParse(args[0], out double epsilon))
+                {
+                    throw new Exception("The provided argument is not a valid decimal number.");
+                }
+
+                Console.WriteLine("Calculating e using limit with provided epsilon: {0}", epsilon);
+
+                double lim_e = ELim(epsilon, 10000);
+
+                double sum_e = Esum(epsilon);
+
+                Console.WriteLine("Calculated value of e: {0}", lim_e);
+
+                Console.WriteLine("Calculated value of e: {0}", sum_e);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+
+        public static double ELim(double epsilon, int maxIterations)
+        {
+            double ePrevious = 0;
+            double eCurrent = 1;
+
+            for (int n = 1; n <= maxIterations && Math.Abs(eCurrent - ePrevious) > epsilon; n++)
+            {
+                ePrevious = eCurrent;
+                eCurrent = Math.Pow(1 + 1.0 / n, n);
+            }
+
+            return eCurrent;
+        }
+        public static double Esum(double epsilon)
+        {
+            double term = 1; double result = 1;
+            ulong n = 1;
+
+            while (term > epsilon)
+            {
+                term = 1f / n;
+                result += 1f / n;
+                n *= ++n;
+                Console.WriteLine(term);
+            }
+
+            return result;
+        }
+        public static double PILim(double epsilon, int maxIterations)
+        {
+            double ePrevious = 0;
+            double eCurrent = 1;
+
+            for (int n = 1; n <= maxIterations && Math.Abs(eCurrent - ePrevious) > epsilon; n++)
+            {
+                ePrevious = eCurrent;
+                eCurrent = Math.Pow(1 + 1.0 / n, n);
+            }
+
+            return eCurrent;
+        }
+        public static double PIsum(double epsilon)
+        {
+            double term = 1; double result = 1;
+            ulong n = 1;
+
+            while (term > epsilon)
+            {
+                term = 1f / n;
+                result += 1f / n;
+                n *= ++n;
+                Console.WriteLine(term);
+            }
+
+            return result;
+        }
+
+        /*Console.Write("Enter const for sum E (no more than 20): ");
+        input = Console.ReadLine();
+        try
+        {
+            if (input == null)
+            {
+                throw new Exception("ERROR");
+            }
+
+
+            // Вторая часть по сумме.
+
+            double sum_e = 0;
+            double term = 1;
+            ulong n = 0;
+            epsilon = Convert.ToDouble(input);
+            while (term > epsilon)
+            {
+                term = 1f / n;
+                sum_e += 1f / n;
+                n++;
+            }
+
+            Console.WriteLine("По сумме: {0}", sum_e);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Произошла ошибка: {e.Message}");
+        }
+
+    }
+    public static ulong factorial(ulong n)
+    {
+        if (n > 20)
+        {
+            throw new ArgumentOutOfRangeException("n", "n should be less than or equal to 20.");
+        }
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        return factorial(n - 1) * n;
+    }*/
+    }
+
+}
--- Lab 1/3 Example/ConsoleApp3/ConsoleApp3/Program.cs
+++ Lab 1/3 Example/ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.IO;
 
 class Program
@@ -55,6 +55,106 @@ class Program
         else
         {
             Console.WriteLine("NULL-ERROR: Введена пустая последовательность символов.");
+        }
+    }
+}*/
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace NumericDataProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа, разделяя их пробелами (для окончания ввода введите 'done'):");
+            string? input = Console.ReadLine();
+            if (input == null) {
+                Console.WriteLine("NULL-ERROR: введена пустая строка");
+            }
+            string[] numberStrings = input.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            List<double> numbers = new List<double>();
+
+            foreach (string numberString in numberStrings)
+            {
+                double number;
+                if (double.TryParse(numberString, NumberStyles.Any, CultureInfo.InvariantCulture, out number))
+                {
+                    numbers.Add(number);
+                }
+                else if (numberString == "done")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Недопустимый символ: " + numberString);
+                    return;
+                }
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("Не было введено ни одного числа.");
+                return;
+            }
+
+            double geometricMean = CalculateGeometricMean(numbers);
+            double harmonicMean = CalculateHarmonicMean(numbers);
+
+            Console.WriteLine("Среднее геометрическое: " + geometricMean.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Среднее гармоническое: " + harmonicMean.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static double CalculateGeometricMean(List<double> numbers)
+        {
+            double product = 1.0;
+            int count = 0;
+
+            foreach (double number in numbers)
+            {
+                // Пропускаем отрицательные числа, так как их нельзя использовать в геометрическом среднем
+                if (number < 0)
+                {
+                    continue;
+                }
+
+                product *= number;
+                count++;
+            }
+
+            if (count == 0 || product == 1.0)
+            {
+                return 0.0; // Если не найдено ни одного неотрицательного числа, вернуть 0
+            }
+
+            return Math.Pow(product, 1.0 / count);
+        }
+
+        static double CalculateHarmonicMean(List<double> numbers)
+        {
+            double sumInverses = 0.0;
+            int count = 0;
+
+            foreach (double number in numbers)
+            {
+                // Пропускаем нулевые числа, так как их нельзя использовать в гармоническом среднем
+                if (number == 0)
+                {
+                    continue;
+                }
+
+                sumInverses += 1.0 / number;
+                count++;
+            }
+
+            if (count == 0)
+            {
+                return 0.0; // Если не найдено ни одного ненулевого числа, вернуть 0
+            }
+
+            return count / sumInverses;
         }
     }
 }
