a6cc6ecb68db91c53c60172d6697bf316b465f3a
NikX <89803680+NikXProG@users.noreply.github.com>
2023-10-24 14:26:11 +0300

    commit

--- Lab 1/1 Example/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lab 1/1 Example/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-
+/*
 namespace Project1
 {
     using System;
@@ -84,4 +84,81 @@ namespace Project1
         }
 
     }
-}
+}*/
+namespace Project1
+{
+    class Program
+    {
+        public struct Result
+        {
+            public int tip;
+            public double p1;
+            public double p2;
+            public double p3;
+        }
+        public static Result Kardano(double a, double b, double c, double d)
+        {
+            Result result = new Result();
+
+            double eps = 1E-14;
+            double p = (3 * a * c - b * b) / (3 * a * a);
+            double q = (2 * b * b * b - 9 * a * b * c + 27 * a * a * d) / (27 * a * a * a);
+            double det = q * q / 4 + p * p * p / 27;
+            if (Math.Abs(det) < eps)
+                det = 0;
+            if (det > 0)
+            {
+                result.tip = 1; // один вещественный, два комплексных корня
+                double u = -q / 2 + Math.Sqrt(det);
+                u = Math.Exp(Math.Log(u) / 3);
+                double yy = u - p / (3 * u);
+                result.p1 = yy - b / (3 * a); // первый корень
+                result.p2 = -(u - p / (3 * u)) / 2 - b / (3 * a);
+                result.p3 = Math.Sqrt(3) / 2 * (u + p / (3 * u));
+            }
+            else
+            {
+                if (det < 0)
+                {
+                    result.tip = 2; // три вещественных корня
+                    double fi;
+                    if (Math.Abs(q) < eps) // q=0
+                        fi = Math.PI / 2;
+                    else
+                    {
+                        if (q < 0) // q<0
+                            fi = Math.Atan(Math.Sqrt(-det) / (-q / 2));
+                        else // q<0
+                            fi = Math.Atan(Math.Sqrt(-det) / (-q / 2)) + Math.PI;
+                    }
+                    double r = 2 * Math.Sqrt(-p / 3);
+                    result.p1 = r * Math.Cos(fi / 3) - b / (3 * a);
+                    result.p2 = r * Math.Cos((fi + 2 * Math.PI) / 3) - b / (3 * a);
+                    result.p3 = r * Math.Cos((fi + 4 * Math.PI) / 3) - b / (3 * a);
+                }
+                else // det=0
+                {
+                    if (Math.Abs(q) < eps)
+                    {
+                        result.tip = 4; // 3-х кратный 
+                        result.p1 = -b / (3 * a); // 3-х кратный 
+                        result.p2 = -b / (3 * a);
+                        result.p3 = -b / (3 * a);
+                    }
+                    else
+                    {
+                        result.tip = 3; // один и два кратных
+                        double u = Math.Exp(Math.Log(Math.Abs(q) / 2) / 3);
+                        if (q < 0)
+                            u = -u;
+                        result.p1 = -2 * u - b / (3 * a);
+                        result.p2 = u - b / (3 * a);
+                        result.p3 = u - b / (3 * a);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+ 
+
