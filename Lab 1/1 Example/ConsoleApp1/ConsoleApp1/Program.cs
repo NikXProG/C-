99884b246238a6a7bac4534b3dec6f616e5771d7
NikX <89803680+NikXProG@users.noreply.github.com>
2023-10-24 03:18:55 +0300

    New commit

--- Lab 1/1 Example/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lab 1/1 Example/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace Project1
+{
+    using System;
+    using static System.Runtime.InteropServices.JavaScript.JSType;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 2, B = -11, C = 12, D = 9 ;
+            double qad = Math.Pow(A, 2); // необходимо умножить все уравнение на A в квадрате для замены.
+
+            // Y3 + Y2 + Y * CA + D * A2 = 0 ищем теперь делители D * A2
+            double free_const = D * qad;
+            for (int i = 1; i <= free_const/2; i++)
+            {
+                if (free_const % i == 0)
+                {
+                    int i1 = i;
+                    int i2 = -i;
+                    Console.WriteLine(i1);
+                    if  ((Math.Pow(i1, 3) + (B * Math.Pow(i1, 2)) + (A * C * i1) + free_const) == 0)
+                    {
+                        // Коэффициенты делимого полинома: 2x^3 - 11x^2 + 12x + 9
+                        double[] dividendCoefficients = { 2, -11, 12, 9 };
+
+                        // Коэффициенты делителя: x + 1/2
+                        double[] divisorCoefficients = {i1 / 2, 1 };
+
+                        int dividendDegree = dividendCoefficients.Length - 1;
+                        int divisorDegree = divisorCoefficients.Length - 1;
+
+                        // Создаем массив для хранения коэффициентов частного
+                        double[] quotientCoefficients = new double[dividendDegree - divisorDegree + 1];
+
+                        // Выполняем долгое деление
+                        for (int q = 0; q < quotientCoefficients.Length; q++)
+                        {
+                            quotientCoefficients[i] = dividendCoefficients[q] / divisorCoefficients[0];
+
+                            for (int j = 0; j <= divisorDegree; j++)
+                            {
+                                dividendCoefficients[q + j] -= quotientCoefficients[q] * divisorCoefficients[j];
+                            }
+                        }
+
+                        Console.WriteLine("Результат деления: ");
+                        Console.WriteLine("Частное: " + string.Join(" ", quotientCoefficients));
+                        Console.WriteLine("Остаток: " + string.Join(" ", dividendCoefficients));
+                    }
+
+                    if ( (Math.Pow(i2, 3) + (B * Math.Pow(i2, 2)) + (A * C * i2) + free_const) == 0)
+                    {
+                        // Коэффициенты делимого полинома: 2x^3 - 11x^2 + 12x + 9
+                        double[] dividendCoefficients = { 2, -11, 12, 9 };
+
+                        // Коэффициенты делителя: x + 1/2
+                        double[] divisorCoefficients = { i2 / 2, 1 };
+
+                        int dividendDegree = dividendCoefficients.Length - 1;
+                        int divisorDegree = divisorCoefficients.Length - 1;
+
+                        // Создаем массив для хранения коэффициентов частного
+                        double[] quotientCoefficients = new double[dividendDegree - divisorDegree + 1];
+
+                        // Выполняем долгое деление
+                        for (int q = 0; q < quotientCoefficients.Length; q++)
+                        {
+                            quotientCoefficients[q] = dividendCoefficients[q] / divisorCoefficients[0];
+
+                            for (int j = 0; j <= divisorDegree; j++)
+                            {
+                                dividendCoefficients[q + j] -= quotientCoefficients[q] * divisorCoefficients[j];
+                            }
+                        }
+
+                        Console.WriteLine("Результат деления: ");
+                        Console.WriteLine("Частное: " + string.Join(" ", quotientCoefficients));
+                        Console.WriteLine("Остаток: " + string.Join(" ", dividendCoefficients));
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Lab 1/5 Example/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Project
+{
+    using System;
+    using System.Linq;
+    using System.Security.Principal;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string? input_string = Console.ReadLine();
+            if (input_string != null) {
+                Console.WriteLine("Вот ваш отсортированная строка первых символов: ");
+                Console.WriteLine( SortAndGetLastCharacters(input_string) );
+                Console.WriteLine("Вот ваша новая строка 1 и последнего символов верхнего и нижнего регистра: ");
+                Console.WriteLine( UpperAndLowerCharacters(input_string) );
+                Console.WriteLine("Вот ваше число слов  введеного вами слова: ");
+                Console.WriteLine(CountWord(input_string));
+                Console.WriteLine("Вот ваша новая строка с замененнными предпоследним словом: ");
+                Console.WriteLine(ReplaceWord(input_string));
+                Console.WriteLine("Вот выше найденное k-слово с заглавной буквой: ");
+                Console.WriteLine(SearchWord(input_string));
+            }
+
+
+        }
+        static string SortAndGetLastCharacters(string input) {
+            string[] words = input.Split(); // разделяем строку на слова
+            Array.Sort(words); // сортируем слова по алфавиту
+            string result = "";
+            foreach (string myString in words)
+            {
+                if (myString.Length > 0) // check if the word is not empty
+                {
+                    result += myString[^1];
+                } 
+            }
+            return result;
+
+        }
+        static string UpperAndLowerCharacters(string input)
+        {
+            string[] words = input.Split(); // разделяем строку на слова
+            string result = "";
+            foreach (string myString in words)
+            {
+                if (myString.Length > 0) // check if the word is not empty
+                {
+                    result += char.ToUpper(myString[0]) + myString.Substring(1, myString.Length - 2) + char.ToLower(myString[^1]) + " ";
+                }
+            }
+            return result;
+
+        }
+        static int CountWord(string input)
+        {
+            string? word = Console.ReadLine();
+            string[] words = input.Split(); // разделяем строку на слова
+            int count = 0;
+            foreach (string myString in words)
+            {
+                if (myString == word)
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+        static string  ReplaceWord(string input)
+        {
+            string? word = Console.ReadLine();
+            string[] words = input.Split(); // разделяем строку на слова
+            string result = "";
+            foreach (string myString in words)
+            {
+                if (myString.Length > 0){
+                    if (myString == words[words.Length - 2])
+                    {
+                        result += word + " ";
+                    }
+                    else
+                    {
+                        result += myString + " ";
+                    }
+                }
+            }
+            return result;
+
+        }
+        static int SearchWord(string input)
+        {
+            string[] words = input.Split(); // разделяем строку на слова
+            Array.Sort(words); // сортируем слова по алфавиту
+            int k = 0;
+            foreach (string myString in words)
+            {
+                if (myString.Length > 0) // check if the word is not empty
+                {
+                    string search = char.IsUpper(myString[0]) + myString.Substring(1, myString.Length - 1);
+                    if ( search == myString)
+                    {
+                        return k;
+                    }
+                    ++k;
+                }
+            }
+            return k;
+
+        }
+
+    }
+}
