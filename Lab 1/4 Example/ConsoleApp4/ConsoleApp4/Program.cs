6fb269573d791bed2b5927a115ff8d1ef7358112
NikX <89803680+NikXProG@users.noreply.github.com>
2023-10-24 14:13:42 +0300

    Commit

---
+++ Lab 1/4 Example/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int periodStart = -1, periodLength = 0;
+            for (int j = 1; j <= resultFrac.Length / 2; j++)
+            {
+                bool isPeriodic = true;
+                for (int l = 0; l < resultFrac.Length / 2; l++)
+                {
+                    if (resultFrac[l] == resultFrac[l + j])
+                    {
+                        for (int n = j; n < resultFrac.Length; n++)
+                        {
+                            if (resultFrac[n] != resultFrac[n - j])
+                            {
+                                isPeriodic = false;
+                                break;
+                            }
+                        }
+
+                        if (isPeriodic == true)
+                        {
+                            periodStart = l;
+                            periodLength = j;
+                            break;
+                        }
+                    }
+
+                }
+                if (periodStart >= 0)
+                {
+                    resultFrac = resultFrac.Substring(0, periodStart) + "(" + resultFrac.Substring(periodStart, periodLength) + ")";
+                }
+            }
+        }
+    }
+}
