8141d05632ed6e60c9a6ac3083257c5794b796cf
NikX <89803680+NikXProG@users.noreply.github.com>
2023-10-26 15:32:25 +0300

    commit

---
+++ Lab 1/6 Example/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program {
+    // delegate Func<double, double> equation берет функцию, которую мы задали и вызывает ее с помощью equation
+    public static double FindRoot(Func<double, double> equation, double lowerBound, double upperBound, double epsilon)
+    {
+        double middle;
+        double fMiddle;
+
+        do
+        {
+            middle = (lowerBound + upperBound) / 2;
+            fMiddle = equation(middle);
+
+            if (fMiddle == 0 || (upperBound - lowerBound) / 2 < epsilon)
+            {
+                return middle;
+            }
+
+            if (equation(lowerBound) * fMiddle < 0)
+            {
+                upperBound = middle;
+            }
+            else
+            {
+                lowerBound = middle;
+            }
+        } while (true);
+    }
+
+    public static void Main(string[] args)
+    {
+        double root = FindRoot(x => x * x - 81, 0, 10, 0);
+        Console.WriteLine("Корень уравнения: " + root);
+    }
+}
